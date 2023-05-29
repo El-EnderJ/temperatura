67b45198b92609b6c81c17bba96ea380f70e7def
El-EnderJ <130647064+El-EnderJ@users.noreply.github.com>
2023-05-29 08:53:39 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,17 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
     basic.showString("C ")
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.acceleration(Dimension.Strength))
+})
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.lightLevel())
     basic.showString("de 300")
+})
+let Pasos = 0
+basic.forever(function () {
+    basic.showNumber(0)
+    if (input.acceleration(Dimension.Strength) > 100) {
+        Pasos += 1
+    }
 })
